2ffdff431351dfc7dddf29d4061dbacf1a8a5ee1
caiquedutra <60185730+caiquedutra@users.noreply.github.com>
2021-05-27 22:24:22 -0300

    Add files via upload

---
+++ sensor_gas.ino
@@ -0,0 +1,89 @@
+#define entradaDigital 52
+#define entradaAnalogica 0
+
+bool dSensor;
+int aSensor;
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(entradaDigital, INPUT);
+}
+
+void loop() {
+  dSensor = digitalRead(entradaDigital);
+  aSensor = analogRead(entradaAnalogica);
+  
+  Serial.print("Leitura entrada digital: ");
+  Serial.println(dSensor);
+  
+  Serial.print("Leitura entrada analógica: ");
+  Serial.println(aSensor);
+  Serial.println();
+  
+  delay(1000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
